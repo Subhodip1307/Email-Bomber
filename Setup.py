7e2605e4594cd254b56c07c5ad6bc351ad82f8c6
Subhodip1307 <sardarsubhodip857@gmail.com>
2023-02-05 14:32:08 +0530

    All requirements

---
+++ Setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+setup(name="Email Bomber", version="1.0",
+description="This tool/program will send a desired amount of email to a target email",
+author="Subhodip Sardar",install_requires=["smtplib"])
